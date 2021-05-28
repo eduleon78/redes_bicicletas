5ff301b928e2c6fadf085f67e24bc2f2cfdf4018
Eduard <eduleon78@gmail.com>
2021-05-28 18:59:37 -0400

    update

--- app.js
+++ app.js
@@ -157,7 +157,9 @@ app.use('/google31dcdb96089a048f', function(req, res){
 app.get('/auth/google',
   passport.authenticate('google', { scope: [
     'https://www.googleapis.com/auth/plus.login',
-    'https://www.googleapis.com/auth/plus.profile.emails.read'
+    'https://www.googleapis.com/auth/plus.profile.emails.read',
+    'profile',
+    'email'
    ],
   })
 );
